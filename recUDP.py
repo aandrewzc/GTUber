39ec2826108652d85aa26a74af1c3041075bee89
Andrew Chen <aandrewc@ucla.edu>
2018-11-08 23:38:47 -0800

    testing mqtt connection

--- recUDP.py
+++ recUDP.py
@@ -1,13 +1,35 @@
 import socket
+import paho.mqtt.client as mqtt
 
-UDP_IP = "192.168.1.113"
-# UDP_IP = socket.gethostname()
+UDP_IP = socket.gethostname()
 UDP_PORT = 5005
+
+# MQTT connection to ensure proper UDP_IP address is used
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Broker connection successful")
+    else:
+        print("Error connecting to broker")
+
+# broker to connect to
+broker = "broker.hivemq.com"
+topic = "ece180d/gtuber/unity_ip" 
+
+print("Creating new instance")
+client = mqtt.Client()
+client.on_connect = on_connect
+
+client.connect(broker)  # connect to broker
+client.loop_start()  # start the loop
+
+print("Subscribing to topic")
+client.subscribe(topic)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" % data)
-    print(addr)
+	print("Publishing IP address")
+	client.publish(topic, UDP_IP)
+	# data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+	# print("received message: %s" % data)
--- sendUDP.py
+++ sendUDP.py
@@ -1,6 +1,41 @@
 import socket
+import paho.mqtt.client as mqtt
 
-UDP_IP = "192.168.1.113"
+global ip_received
+
+# broker to connect to
+broker = "broker.hivemq.com"
+topic = "ece180d/gtuber/unity_ip" 
+
+# callback function for connection
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Broker connection successful")
+    else:
+        print("Error connecting to broker")
+
+# callback for messages
+def on_message(client, userdata, message):
+    print("%s" % (str(message.payload.decode("utf-8"))))
+    UDP_IP = str(message.payload.decode("utf-8"))
+
+
+print("Creating new instance")
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+print("Connecting to broker: %s" % broker)
+client.connect(broker)
+client.loop_start()
+
+print("Subscribing to topic: %s" % topic)
+client.subscribe(topic)
+
+client.loop_forever()
+
+
+# UDP_IP = "192.168.1.113"
 UDP_PORT = 5005
 
 addr = (UDP_IP, UDP_PORT)
