e4e83152294b01f55ffe0c46a4f8f1603616952b
sjsims55 <44824954+sjsims55@users.noreply.github.com>
2018-12-11 20:23:32 -0800

    Update dist_vid_color.py

--- dist_vid_color.py
+++ dist_vid_color.py
@@ -34,15 +34,15 @@ def filter_to_color (frame):
 	
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
-	lower_red = np.array([0,0,0]) 
-	upper_red = np.array([50,255,50])
+	lower_red = np.array([0,30,0]) 
+	upper_red = np.array([120,255,50])
 	mask = cv2.inRange(hsv, lower_red, upper_red) 
 		# Display the resulting frame
 	res = cv2.bitwise_and(frame,frame, mask= mask) 
 
 		
 	boundaries = [
-		([0, 0, 0], [50, 255, 50])]
+		([0, 30, 0], [120, 255, 50])]
 
 	for (lower, upper) in boundaries:
 			# create NumPy arrays from the boundaries
@@ -59,61 +59,82 @@ def filter_to_color (frame):
 	return output
 	
 	
-	
+def	drunkTest():
 
-# initialize the known distance from the camera to the object, which
-# in this case is 24 inches
-KNOWN_DISTANCE = 24.0
- 
-# initialize the known object width, which in this case, the piece of
-# paper is 12 inches wide
-KNOWN_WIDTH = 9.0
- 
-# load the furst image that contains an object that is KNOWN TO BE 2 feet
-# from our camera, then find the paper marker in the image, and initialize
-# the focal length
-#image = cv2.imread("images/2ft.png")
-cap = cv2.VideoCapture(0)
+	# initialize the known distance from the camera to the object, which
+	# in this case is 24 inches
+	KNOWN_DISTANCE = 24.0
+	 
+	# initialize the known object width, which in this case, the piece of
+	# paper is 12 inches wide
+	KNOWN_WIDTH = 9.0
+	 
+	# load the furst image that contains an object that is KNOWN TO BE 2 feet
+	# from our camera, then find the paper marker in the image, and initialize
+	# the focal length
+	#image = cv2.imread("images/2ft.png")
 
-while(True):
-	try:
-		ret, frame = cap.read()
-		output = filter_to_color(frame)
-		marker = find_marker(output)
-		break
-	except:
-		pass
+	desiredDistance = 84;
+	returnThreshold  = 24;
 
-focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
-while(True):
+	cap = cv2.VideoCapture(0)
 
 	while(True):
 		try:
 			ret, frame = cap.read()
 			output = filter_to_color(frame)
 			marker = find_marker(output)
+			print(marker[1][0])
 			break
 		except:
 			pass
-	
-	
-	
-	inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
-	#box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
-	
-	#if imutils.is_cv2()
-	#	box = cv2.cv.BoxPoints(marker)
-	#else
-	#	box = cv2.boxPoints(marker)	
 
-	box = cv2.boxPoints(marker)
-	box = np.int0(box)
-	cv2.drawContours(output, [box], -1, (0, 255, 0), 2)
-	cv2.putText(output, "%.2fft" % (inches / 12), (output.shape[1] - 200, output.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
-	cv2.imshow('output',output) 
+	focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+	distanceNotReached = True;
+	returnNotReached = True;
+	while(distanceNotReached or returnNotReached):
+	
+		while(True):
+			try:
+				ret, frame = cap.read()
+				output = filter_to_color(frame)
+				marker = find_marker(output)
+				break
+			except:
+				pass
+		
+		
+		
+		inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+		
+		if(distanceNotReached == False):
+			if inches < returnThreshold:
+				returnNotReached = False
+		if inches > desiredDistance:
+			distanceNotReached = False
+		
+		
+		
+		
+		#box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
+		
+		#if imutils.is_cv2()
+		#	box = cv2.cv.BoxPoints(marker)
+		#else
+		#	box = cv2.boxPoints(marker)	
 
-cap.release()
-cv2.destroyAllWindows()
+		box = cv2.boxPoints(marker)
+		box = np.int0(box)
+		cv2.drawContours(output, [box], -1, (0, 255, 0), 2)
+		cv2.putText(output, "%.2fft" % (inches / 12), (output.shape[1] - 200, output.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
+		cv2.imshow('output',output) 
+
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+	cap.release()
+	cv2.destroyAllWindows()
+	return
+	
+drunkTest()
